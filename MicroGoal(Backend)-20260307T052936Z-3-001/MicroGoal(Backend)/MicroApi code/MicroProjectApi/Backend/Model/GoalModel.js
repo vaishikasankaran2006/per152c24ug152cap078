bd165766d8b4ff3ef57d6dd7c68d12da8b432789
vaishikasankaran2006 <vaishikasankaran2006@gmail.com>
2026-03-07 19:31:58 +0530

    Delete MicroGoal(Backend)-20260307T052936Z-3-001/MicroGoal(Backend) directory

--- MicroGoal(Backend)-20260307T052936Z-3-001/MicroGoal(Backend)/MicroApi code/MicroProjectApi/Backend/Model/GoalModel.js
+++
@@ -1,7 +0,0 @@
-const goalSchema = require("../Schema/GoalSchema");
-const mongoose = require("mongoose");
-const model = mongoose.model;
-
-const goalModel = new model("goal", goalSchema);
-
-module.exports = goalModel;
